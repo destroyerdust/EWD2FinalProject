a7262dd52ddc64eecb772614ca500559de36c90d
Sean Barkley <sean@seanbarkley.com>
2015-05-16 10:51:41 -0400

    Made it say something useful

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-echo "Hello!";
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+echo "Hello World. I will use you soon.";
+
+$conn -> close();
 ?>
